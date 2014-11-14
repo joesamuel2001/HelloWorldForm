1ac3fdc0c2e81d65a7162137e8961714a04f4b87
AHC-RAmeti <reshat.ameti@aurora.org>
2014-11-14 11:21:18 -0600

    Another line of code added from GitHub

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -24,6 +24,7 @@ namespace HelloWorld
             lblHello.Text = "Hello World, we're back";
             lblHello.Text = "Hello World, we're not done yet."; 
             lblHello.Text = "Hello World, text added from GitHub";
+            lblHello.Text = "Hello World, another line added from GitHub";
         }
     }
 }
